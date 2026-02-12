76123dcab974927c5f6f719b8aca25ff831c17f0
rileywallaceriley <67809682+rileywallaceriley@users.noreply.github.com>
2026-02-12 00:47:01 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,44 +1,44 @@
-const CACHE_NAME = 'northern-dial-v1';
+const CACHE_NAME = ‘northern-dial-v1’;
 const urlsToCache = [
-  '/',
-  '/index.html',
-  'https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Oswald:wght@700&family=Roboto+Condensed:wght@400;700&display=swap',
-  'https://i.imgur.com/XIAPd0N.png',
-  'https://i.imgur.com/LJFSC1l.jpeg',
-  'https://i.imgur.com/38QC8zh.jpeg',
-  'https://i.imgur.com/7X7rN10.jpeg'
+‘./’,
+‘./index.html’,
+‘https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Oswald:wght@700&family=Roboto+Condensed:wght@400;700&display=swap’,
+‘https://i.imgur.com/XIAPd0N.png’,
+‘https://i.imgur.com/LJFSC1l.jpeg’,
+‘https://i.imgur.com/38QC8zh.jpeg’,
+‘https://i.imgur.com/7X7rN10.jpeg’
 ];
 
 // Install service worker and cache assets
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener(‘install’, event => {
+event.waitUntil(
+caches.open(CACHE_NAME)
+.then(cache => cache.addAll(urlsToCache))
+);
 });
 
 // Serve cached content when offline
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // Return cached version or fetch from network
-        return response || fetch(event.request);
-      })
-  );
+self.addEventListener(‘fetch’, event => {
+event.respondWith(
+caches.match(event.request)
+.then(response => {
+// Return cached version or fetch from network
+return response || fetch(event.request);
+})
+);
 });
 
 // Clean up old caches
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
+self.addEventListener(‘activate’, event => {
+event.waitUntil(
+caches.keys().then(cacheNames => {
+return Promise.all(
+cacheNames.map(cacheName => {
+if (cacheName !== CACHE_NAME) {
+return caches.delete(cacheName);
+}
+})
+);
+})
+);
 });
