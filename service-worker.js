49ff2bfc9ee44106efba8124dafb6b4490f6e1c5
rileywallaceriley <67809682+rileywallaceriley@users.noreply.github.com>
2026-09-11 23:12:14 -0400

    Prevent stale PWA navigation on homepage and blog

--- service-worker.js
+++ service-worker.js
@@ -1,44 +1,46 @@
-const CACHE_NAME = ‘northern-dial-v1’;
-const urlsToCache = [
-‘./’,
-‘./index.html’,
-‘https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Oswald:wght@700&family=Roboto+Condensed:wght@400;700&display=swap’,
-‘https://i.imgur.com/XIAPd0N.png’,
-‘https://i.imgur.com/LJFSC1l.jpeg’,
-‘https://i.imgur.com/38QC8zh.jpeg’,
-‘https://i.imgur.com/7X7rN10.jpeg’
+const CACHE_NAME = 'northern-dial-v2';
+const STATIC_ASSETS = [
+  './',
+  './index.html',
+  'https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Oswald:wght@700&family=Roboto+Condensed:wght@400;700&display=swap',
+  'https://i.imgur.com/XIAPd0N.png'
 ];
 
-// Install service worker and cache assets
-self.addEventListener(‘install’, event => {
-event.waitUntil(
-caches.open(CACHE_NAME)
-.then(cache => cache.addAll(urlsToCache))
-);
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(STATIC_ASSETS))
+  );
 });
 
-// Serve cached content when offline
-self.addEventListener(‘fetch’, event => {
-event.respondWith(
-caches.match(event.request)
-.then(response => {
-// Return cached version or fetch from network
-return response || fetch(event.request);
-})
-);
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    Promise.all([
+      caches.keys().then(names => Promise.all(
+        names.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
+      )),
+      self.clients.claim()
+    ])
+  );
 });
 
-// Clean up old caches
-self.addEventListener(‘activate’, event => {
-event.waitUntil(
-caches.keys().then(cacheNames => {
-return Promise.all(
-cacheNames.map(cacheName => {
-if (cacheName !== CACHE_NAME) {
-return caches.delete(cacheName);
-}
-})
-);
-})
-);
+self.addEventListener('fetch', event => {
+  const request = event.request;
+
+  if (request.mode === 'navigate') {
+    event.respondWith(
+      fetch(request)
+        .then(response => {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(request, copy));
+          return response;
+        })
+        .catch(() => caches.match(request).then(cached => cached || caches.match('./index.html')))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(request).then(cached => cached || fetch(request))
+  );
 });
